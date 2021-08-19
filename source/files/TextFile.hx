67bc71cbb02179137260d930af9310d43e31440b
Knose <os7138ce-s@student.lu.se>
2021-08-19 12:10:30 +0200

    Issue fixed, language server error

--- source/files/TextFile.hx
+++ source/files/TextFile.hx
@@ -1,10 +1,16 @@
 package files;
 
+import files.FileFactory.FileType;
+
 class TextFile extends BaseFile
 {
+	var fileType:FileType;
+
 	public function new(x:Float, y:Float, values:Null<Dynamic>)
 	{
 		super(x, y, values);
+
+		this.fileType = FileType.Text;
 
 		// GRAPHICS
 		loadGraphic("assets/images/file.png", false, Global.CELL_SIZE, Global.CELL_SIZE);
