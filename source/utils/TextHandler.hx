f0ee430cadc7c5c16cdc35e7811246866c8857c8
Oscar Cederberg <os7138ce-s@student.lu.se>
2022-05-25 19:01:13 +0200

    Add newlines handling

--- source/utils/TextHandler.hx
+++ source/utils/TextHandler.hx
@@ -16,13 +16,20 @@ class TextHandler
     public function format(charsPerLine:Int):Array<String> 
     {
         var processing:String = unformatted;
-        processing = processing.replace("\r", "").replace("\t", "");
+        processing = processing.replace("\r", "").replace("\t", "").trim();
 
         while (processing.length > charsPerLine)
         {
-            if (processing.charAt(charsPerLine - 2) != " " &&
-                processing.charAt(charsPerLine - 1) != " " &&
-                processing.charAt(charsPerLine    ) != " ")
+            var newlinePos = processing.indexOf("\n");
+
+            if (newlinePos != -1 && newlinePos < charsPerLine)
+            {
+                formatted.push(processing.substring(0, newlinePos));
+                processing = processing.substring(newlinePos + 1);
+            }
+            else if (processing.charAt(charsPerLine - 2) != " " &&
+                     processing.charAt(charsPerLine - 1) != " " &&
+                     processing.charAt(charsPerLine    ) != " ")
             {
                 formatted.push(processing.substring(0, charsPerLine - 1) + "-");
                 processing = processing.substring(charsPerLine - 1);
@@ -40,6 +47,14 @@ class TextHandler
                 processing = processing.substring(charsPerLine);
             }
         }
+        
+        while (processing.length > 0 && processing.contains("\n"))
+        {
+            var index = processing.indexOf("\n");
+            formatted.push(processing.substring(0, index));
+            processing = processing.substring(index + 1);
+        }
+        
         if (processing.length > 0)
         {
             formatted.push(processing);
